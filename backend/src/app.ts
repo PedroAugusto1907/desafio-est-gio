e5a7d3b3e961c4806368b1679e596a1437dd848c
Pedro Albuquerque <pedroaugusto19017@gmail.com>
2026-06-30 20:43:21 -0300

    feat: adiciona middleware de log de requisicoes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import accountRoutes from './routes/accountRoutes';
 import { errorHandler } from './middlewares/errorHandler';
+import { requestLogger } from './middlewares/requestLogger';
 
 const app = express();
 
+app.use(requestLogger);
 app.use(express.json());
 app.use('/api', accountRoutes);
 app.use(errorHandler);
