6702f2f1b991fee8463794659f8947ed09b1512c
Pedro Albuquerque <pedroaugusto19017@gmail.com>
2026-06-30 21:16:51 -0300

    feat: habilita CORS no backend para consumo pelo frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import accountRoutes from './routes/accountRoutes';
 import { errorHandler } from './middlewares/errorHandler';
 import { requestLogger } from './middlewares/requestLogger';
 
 const app = express();
 
+app.use(cors());
 app.use(requestLogger);
 app.use(express.json());
 app.use('/api', accountRoutes);
